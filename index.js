c5235382a964484e583df7d02e2c7ed66715a76e
André Valverde <devandrecontact@gmail.com>
2025-01-31 10:38:36 -0300

    feat: created model to new template

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ async function getTemplateHeader() {
 
   div.innerHTML = header;
 
-  return div.querySelector("#headerTemplate").content;
+  return div.querySelector("headerTemplate").content;
 }
 
 async function updateComponentDivHeader() {
   const element = await getTemplateHeader();
 
   document.getElementById("header").appendChild(element.cloneNode(true));
+}
+
+async function getTemplateMyValues() {
+  const response = await fetch("myvalues.html");
+  const myvalues = await response.text();
+  const div = document.createElement("div");
+
+  div.innerHTML = myvalues;
+
+  return div.querySelector("container-myvalues").content;
+}
+
+async function getJsonMyValues() {
+  const response = await fetch("myvalues.json");
+  const myvalues = await response.json();
+  const div = document.createElement("div");
+
+  div.innerHTML = myvalues;
+
+  return div.querySelector("container-myvalues").content;
 }
 
 updateComponentDivHeader();
